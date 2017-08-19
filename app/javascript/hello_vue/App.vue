8dfd94290635afab1624ef3d3d65fc83ecd8bd1d
Rubén Ibáñez Carmona <ruben.ibc@gmail.com>
2017-08-19 10:42:17 +0200

    different tries to pass data

--- app/javascript/hello_vue/App.vue
+++ app/javascript/hello_vue/App.vue
@@ -1,17 +1,25 @@
 <template>
   <div id="app">
     <p>{{ message }}</p>
+    <p>{{ i18n.hello }}</p>
+    <p>{{ i18n.bye }}</p>
   </div>
 </template>
 
 <script>
-export default {
-  data: function () {
-    return {
-      message: "Hello Vue!"
+    export default {
+	props: {
+	    i18n: {
+		type: Object,
+		required: true
+	    }
+	},
+	data: function () {
+	    return {
+		message: "Hello Vue!"
+	    }
+	}
     }
-  }
-}
 </script>
 
 <style scoped>
--- app/javascript/hello_vue/index.js
+++ app/javascript/hello_vue/index.js
@@ -1,33 +1,34 @@
-/* eslint no-console: 0 */
-// Run this example by adding <%= javascript_pack_tag 'hello_vue' %> and
-// <%= stylesheet_pack_tag 'hello_vue' %> to the head of your layout file,
-// like app/views/layouts/application.html.erb.
-// All it does is render <div>Hello Vue</div> at the bottom of the page.
-
 import Vue from 'vue'
-import store from './store'
+import Vuex from 'vuex';
+// import store from './store'
 import moment from 'moment-timezone'
+import Axios from 'axios';
+import TurbolinksAdapter from 'vue-turbolinks'
+
 moment.tz.setDefault('UTC')
 Object.defineProperty(Vue.prototype, '$moment', { get() { return this.$root.moment } })
-// import Vuex from 'vuex'
-import TurbolinksAdapter from 'vue-turbolinks'
+
+Vue.use(TurbolinksAdapter)
+Vue.use(Vuex)
 
 import App from './App.vue'
 
-Vue.use(TurbolinksAdapter)
-// Vue.use(Vuex)
-
 document.addEventListener('turbolinks:load', () => {
-  // const app = new Vue(App).$mount('hello-vue')
-  const app = new Vue({
-    el: 'hello-vue',
-    render: h => h(App),
-    data: {
-      moment
-    },
-    // store,
-    components: {
-      App
+    // const app = new Vue(App).$mount('hello-vue')
+    const node = document.querySelector('hello-vue')
+
+    if (node != null) {
+	const props = JSON.parse(node.getAttribute('data'))
+	const app = new Vue({
+	    el: 'hello-vue',
+	    render: h => h(App, { props }),
+	    data: {
+		moment
+	    },
+	    // store,
+	    // components: {
+	    // 	App
+	    // }
+	})
     }
-  })
 })
